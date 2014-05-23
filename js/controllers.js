654436da35dc02e7ed59b8fa818010fbc4ccd7ac
pjh5 <pjh5@rice.edu>
2014-05-23 17:32:51 -0500

    made button

--- js/controllers.js
+++ js/controllers.js
@@ -176,7 +176,7 @@ mapApp.controller('SearchCtrl', function($scope, $http, $window, $timeout, tempS
 			mapElement.website + '<br />' + 
 			mapElement.hours +
 			mapElement.bus + 
-			'<div compile-data template="{{<button type="button" class="btn btn-default" ng-click="glueToMap()">Glue to Map</button>}}"></div>' +
+			'<button type="button" class="btn btn-default" ng-click="glueToMap()">Glue to Map</button>' +
 			'</div>'; // Added content to info thing
 			
 			var infoWindow = new google.maps.InfoWindow({
